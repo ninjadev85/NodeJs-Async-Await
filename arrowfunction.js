af8649dcc26f0adb06928d46b46fda853b3c4646
Vu Dao <ninjadev85@gmail.com>
2017-05-10 14:30:47 +0700

    demo function

--- arrowfunction.js
+++ arrowfunction.js
@@ -1,8 +1,9 @@
 let mang = [4,5,6,3,8,9];
 
-mang.forEach(e=>console.log(e));
+//mang.forEach(e=>console.log(e));
 
-var mang2 = mang.map(e=> e*2) 
+var nhan2 = e=>e*2;
+var mang2 = mang.map(nhan2) 
 
 console.log(mang2);
 
@@ -12,4 +13,17 @@ function add(a,b){
 
 let add1 = (a,b) =>  a+b;
 
-console.log(add1(4,5));
+//console.log(add1(4,5));
+
+// function return a function
+let getFunction =(number)=>{
+    if(number > 0 ){
+        return ()=>console.log('so duong')
+    }
+    return ()=>console.log('so am')
+}
+getFunction(-1)(); // excute a function
+
+let a=()=>{}
+
+console.log(a());
